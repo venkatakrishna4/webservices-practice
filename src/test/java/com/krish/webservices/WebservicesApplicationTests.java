ead241774a9f95400cba376bd742ed549227fb02
Venkata Krishna Singamsetti <venkata.krishnakloudspot.com@Venkatas-MBP.bbrouter>
2022-11-21 21:16:40 +0530

    Initial Commit

---
+++ src/test/java/com/krish/webservices/WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krish.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
